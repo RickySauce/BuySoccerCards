ff0e72c2cd1c427689914160d137355996365fd2
Eric DeSimone <ericdesimone@Erics-MBP.home>
2021-08-17 11:12:46 -0400

    established connection to db with mongoose

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ const express = require('express');
 const mongoose = require("mongoose")
 const bodyParser = require("body-parser")
 
+mongoose.connect("mongodb://localhost:27017/buysoccercardsdb", {
+  useNewUrlParser: "true",
+})
+mongoose.connection.on("error", err => {
+  console.log("err", err)
+})
+mongoose.connection.on("connected", (err, res) => {
+  console.log("mongoose is connected")
+})
+
 const app = express();
 
 const port = process.env.PORT || 5000;
