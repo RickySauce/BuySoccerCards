47d02289cc91912c5b38ef33945a84982811beba
Eric DeSimone <eric.desimone@cortland.edu>
2021-09-03 08:43:06 -0400

    created public folder in src, moved images and subsequent folders/files

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {
   Switch,
   Route,
 } from "react-router-dom";
-import Header from './header'
+import Header from './components/header'
 
 const App = () => {
   return (
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,9 +1,12 @@
 import React from 'react' 
 import Navbar from './navbar'
+const logo = require('/public/imgs/logos/Buy_Soccer_Cards_Logo_Final_300x.png')
+
 
 const Header = () => {
     return(
         <div>
+            <img src={logo.default}></img>
             <Navbar/>
         </div>
     )
