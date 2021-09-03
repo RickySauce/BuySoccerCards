f1a82e73dadd8a928762711ba378136e8f8b2846
Eric DeSimone <eric.desimone@cortland.edu>
2021-09-03 08:54:20 -0400

    set up configuration for babel module resolver and vscode, now able to require absolute paths within the src directory

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,6 +1,6 @@
 import React from 'react' 
 import Navbar from './navbar'
-const logo = require('/public/imgs/logos/Buy_Soccer_Cards_Logo_Final_300x.png')
+const logo = require('src/public/imgs/logos/Buy_Soccer_Cards_Logo_Final_300x.png')
 
 
 const Header = () => {
