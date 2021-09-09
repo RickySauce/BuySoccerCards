38e81e42303a00889d5ee489fef5df8b3941be22
Eric DeSimone <eric.desimone@cortland.edu>
2021-09-09 17:47:06 -0400

    moved links into li tags

--- src/components/header/navbar/index.js
+++ src/components/header/navbar/index.js
@@ -7,16 +7,16 @@ const Navbar = () => {
     return(
         <div>
             <nav>
-                <Link to='/'>HOME</Link>
-                <Link to='/about-us'>ABOUT US</Link>
-                <Link to='/reviews'>REVIEWS</Link>
-                <Link to='/blog'>BLOG</Link>
-                <Link to='/all-products'>ALL PRODUCTS</Link>
-                <Link to='/single-cards'>RAW SINGLE CARDS</Link>
-                <Link to='/graded-cards'>GRADED SOCCER CARDS</Link>
-                <Link to='/hobby-boxes'>HOBBY BOXES</Link>
-                <Link to='/grading-service'>EXPERT GRADING ADVICE</Link>
-                <Link to='/trading-card-stands'>TRADING CARD STANDS</Link>
+                <li><Link to='/'>HOME</Link></li>
+                <li><Link to='/about-us'>ABOUT US</Link></li> 
+                <li><Link to='/reviews'>REVIEWS</Link></li>
+                <li><Link to='/blog'>BLOG</Link></li>
+                <li><Link to='/all-products'>ALL PRODUCTS</Link></li>
+                <li><Link to='/single-cards'>RAW SINGLE CARDS</Link></li>
+                <li><Link to='/graded-cards'>GRADED SOCCER CARDS</Link></li>
+                <li><Link to='/hobby-boxes'>HOBBY BOXES</Link></li>
+                <li><Link to='/grading-service'>EXPERT GRADING ADVICE</Link></li>
+                <li><Link to='/trading-card-stands'>TRADING CARD STANDS</Link><Link to='/trading-card-stands'>TRADING CARD STANDS</Link></li>
             </nav>
         </div>
     )
