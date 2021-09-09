f1c3ed6b2e77af2f6c5e4b3e36e774c06d02bf2d
Eric DeSimone <eric.desimone@cortland.edu>
2021-09-09 09:48:54 -0400

    added css file for Header, created seperate component for logo, added styling for logo

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,12 +1,20 @@
 import React from 'react' 
 import Navbar from './navbar'
+import './header.css'
 const logo = require('src/public/imgs/logos/Buy_Soccer_Cards_Logo_Final_300x.png')
 
+const LogoContainer = () => {
+    return (
+        <div className="container">
+            <img id="logo" src={logo.default}></img>
+        </div>
+    )
+}
 
 const Header = () => {
     return(
         <div>
-            <img src={logo.default}></img>
+            <LogoContainer/>
             <Navbar/>
         </div>
     )
