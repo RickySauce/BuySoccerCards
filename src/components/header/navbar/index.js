1f7c1929852fb7be4387881ffc095094d74c136f
Eric DeSimone <eric.desimone@cortland.edu>
2021-09-10 09:35:57 -0400

    added additional margin to links, and padding to navbar

--- src/components/header/navbar/index.js
+++ src/components/header/navbar/index.js
@@ -5,18 +5,18 @@ import {
 
 const Navbar = () => {
     return(
-        <div>
+        <div id="navbar">
             <nav className="container centered">
-                <li className="link"><Link to='/about-us'>ABOUT US</Link></li> 
-                <li className="link"><Link to='/'>HOME</Link></li>
-                <li className="link"><Link to='/reviews'>REVIEWS</Link></li>
-                <li className="link"><Link to='/blog'>BLOG</Link></li>
-                <li className="link"><Link to='/all-products'>ALL PRODUCTS</Link></li>
-                <li className="link"><Link to='/single-cards'>RAW SINGLE CARDS</Link></li>
-                <li className="link"><Link to='/graded-cards'>GRADED SOCCER CARDS</Link></li>
-                <li className="link"><Link to='/hobby-boxes'>HOBBY BOXES</Link></li>
-                <li className="link"><Link to='/grading-service'>EXPERT GRADING ADVICE</Link></li>
-                <li className="link"><Link to='/trading-card-stands'>TRADING CARD STANDS</Link></li>
+                    <li className="link"><Link to='/about-us'>ABOUT US</Link></li> 
+                    <li className="link"><Link to='/'>HOME</Link></li>
+                    <li className="link"><Link to='/reviews'>REVIEWS</Link></li>
+                    <li className="link"><Link to='/blog'>BLOG</Link></li>
+                    <li className="link"><Link to='/all-products'>ALL PRODUCTS</Link></li>
+                    <li className="link"><Link to='/single-cards'>RAW SINGLE CARDS</Link></li>
+                    <li className="link"><Link to='/graded-cards'>GRADED SOCCER CARDS</Link></li>
+                    <li className="link"><Link to='/hobby-boxes'>HOBBY BOXES</Link></li>
+                    <li className="link"><Link to='/grading-service'>EXPERT GRADING ADVICE</Link></li>
+                    <li className="link"><Link to='/trading-card-stands'>TRADING CARD STANDS</Link></li> 
             </nav>
         </div>
     )
