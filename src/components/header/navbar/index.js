fe35150ffecc653aa3d50994013275ba3fa603ae
Eric DeSimone <eric.desimone@cortland.edu>
2021-09-10 08:55:14 -0400

    added additional styling for project wide links, and the main navbar

--- src/components/header/navbar/index.js
+++ src/components/header/navbar/index.js
@@ -6,17 +6,17 @@ import {
 const Navbar = () => {
     return(
         <div>
-            <nav>
-                <li><Link to='/'>HOME</Link></li>
-                <li><Link to='/about-us'>ABOUT US</Link></li> 
-                <li><Link to='/reviews'>REVIEWS</Link></li>
-                <li><Link to='/blog'>BLOG</Link></li>
-                <li><Link to='/all-products'>ALL PRODUCTS</Link></li>
-                <li><Link to='/single-cards'>RAW SINGLE CARDS</Link></li>
-                <li><Link to='/graded-cards'>GRADED SOCCER CARDS</Link></li>
-                <li><Link to='/hobby-boxes'>HOBBY BOXES</Link></li>
-                <li><Link to='/grading-service'>EXPERT GRADING ADVICE</Link></li>
-                <li><Link to='/trading-card-stands'>TRADING CARD STANDS</Link><Link to='/trading-card-stands'>TRADING CARD STANDS</Link></li>
+            <nav className="container centered">
+                <li className="link"><Link to='/about-us'>ABOUT US</Link></li> 
+                <li className="link"><Link to='/'>HOME</Link></li>
+                <li className="link"><Link to='/reviews'>REVIEWS</Link></li>
+                <li className="link"><Link to='/blog'>BLOG</Link></li>
+                <li className="link"><Link to='/all-products'>ALL PRODUCTS</Link></li>
+                <li className="link"><Link to='/single-cards'>RAW SINGLE CARDS</Link></li>
+                <li className="link"><Link to='/graded-cards'>GRADED SOCCER CARDS</Link></li>
+                <li className="link"><Link to='/hobby-boxes'>HOBBY BOXES</Link></li>
+                <li className="link"><Link to='/grading-service'>EXPERT GRADING ADVICE</Link></li>
+                <li className="link"><Link to='/trading-card-stands'>TRADING CARD STANDS</Link></li>
             </nav>
         </div>
     )
