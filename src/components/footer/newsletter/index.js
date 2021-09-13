6b71bafc5858ceae00e6df7e4f4b805aca9e7fff
Eric DeSimone <eric.desimone@cortland.edu>
2021-09-13 15:08:51 -0400

    started working on newsletter

--- src/components/footer/newsletter/index.js
+++ src/components/footer/newsletter/index.js
@@ -1,9 +1,42 @@
 import React from 'react'
 
+const NewsLetterContent = () =>{
+    return(
+        <div>
+            <h2> newsletter </h2>
+            <div>
+                <p>
+                     Want to be the first to know when new rare cards are available? Sign up now for the latest updates!
+                </p>
+            </div>
+        </div>
+    )
+}
+
+const NewsLetterField = () => {
+    return(
+        <div>
+            <form method="post" accept-charset="UTF-8" className="contact-form">  
+                <div>
+                    <div>
+                        <label for="Email" >Email</label>                    
+                    </div>
+                    <div >
+                        <button type="submit" name="commit">Subscribe</button>
+                    </div>
+                </div>
+             </form>
+        </div>
+    )
+}
+
 const Newsletter = () => {
     return(
         <div>
-            newsletter
+            <div className="container">
+                <NewsLetterContent/>
+                <NewsLetterField/>
+            </div>
         </div>
     )
 }
