440e4cdfe8298cf50b7fc25c5488fc859b6c186c
Eric DeSimone <eric.desimone@cortland.edu>
2021-09-14 12:59:57 -0400

    added all products page/component

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import AboutUs from './pages/about-us';
 import {
     Switch,
     Route
@@ -9,7 +10,7 @@ const Main = () => {
     return(
         <div className="main">
             <Switch>
-                
+
             </Switch>
         </div>
     )
