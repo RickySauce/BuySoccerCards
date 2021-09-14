3814c2cde3c23e0b65753adcd806a128c3a49b33
Eric DeSimone <eric.desimone@cortland.edu>
2021-09-14 13:01:52 -0400

    added expert grading advice page/component

---
+++ src/components/main/pages/expert-grading-advice/index.js
@@ -0,0 +1,11 @@
+import React from 'react' 
+
+const ExpertGradingAdvice = () => {
+    return(
+        <div>
+            expert grading advice
+        </div>
+    )
+}
+
+export default ExpertGradingAdvice
